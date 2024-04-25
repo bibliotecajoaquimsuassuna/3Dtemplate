59de30d5199781d5ac3220ac7b36a348f1a8e37c
Biblioteca <Biblioteca@D_012059.demprel.local>
2024-04-25 17:34:43 -0300

    adding model to animate

--- Assets/Game/Scripts/Gameplay/Movement.cs
+++ Assets/Game/Scripts/Gameplay/Movement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Animator anim;
+    [SerializeField] CharacterController controller;
+    [SerializeField] float areaAtk;
+    List<Transform> enemyList = new List<Transform>();
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine("Kick");
+        }
+
+    }
+
+    //Attacks
+    IEnumerator Kick()
+    {
+        anim.SetBool("isKicking", true);
+        yield return new WaitForSeconds(1.2f);
+        GetEnemiesInRange();
+        anim.SetBool("isKicking", false);
+    }
+
+    void GetEnemiesInRange()
+    {
+        enemyList.Clear();
+        foreach(Collider colisor in Phisics.OverlapSphere((transform.position + transform.forward), areaAtk))
+        {
+
+        }
+    }
+
+    //Movements
+    void Run()
+    {
+
     }
 }
